a0b68ffed8501687fb69031d744c5b9866683644
Anmol Shah <anmolsha80@gmail.com>
2024-10-15 19:18:10 +0545

    Resolve open threads after PR review

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -16,4 +16,23 @@ const INITIAL_ITEMS = [
   },
 ];
 
-export { INITIAL_ITEMS };
+const SORT_OPTIONS = [
+  {
+    label: 'Sort by default',
+    value: 'default',
+  },
+  {
+    label: 'Sort by name',
+    value: 'name',
+  },
+  {
+    label: 'Sort by packed',
+    value: 'packed',
+  },
+  {
+    label: 'Sort by unpacked',
+    value: 'unpacked',
+  },
+];
+
+export { INITIAL_ITEMS, SORT_OPTIONS };
