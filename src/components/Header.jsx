1a2cd65d8a729a8491986da17b1f671b5af22163
Anmol Shah <anmolsha80@gmail.com>
2024-10-17 00:14:40 +0545

    features/install-and-configure-zustand - Install and configure zustand to manage the state instead of using context API - Create a zustand itemsStore to place all the implementations for the methods such as add, delete, toggle, mark and reset items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
+import { useItemsStore } from '@/stores/itemsStore';
+
 import Logo from '@/components/Logo';
 import Counter from '@/components/Counter';
 
 const Header = () => {
+  const items = useItemsStore((state) => state.items);
+
+  const numberOfItemsPacked = items.filter((item) => item.packed).length;
+
+  const totalNumberOfItems = items.length;
+
   return (
     <header>
       <Logo />
-      <Counter />
+      <Counter
+        numberOfItemsPacked={numberOfItemsPacked}
+        totalNumberOfItems={totalNumberOfItems}
+      />
     </header>
   );
 };
